399a1ad26e731e8b024bb8f9985732b070e18812
CarlosCastaneda04 <carlos.castaneda1@catolica.edu.sv>
2024-08-23 04:06:38 -0600

    notas se actualizan

--- app/Http/Controllers/DocenteController.php
+++ app/Http/Controllers/DocenteController.php
@@ -29,60 +29,95 @@ class DocenteController extends Controller
 
 
     public function verNotasAlumno($materia_id, $alumno_id) {
-        $notas = DB::select('
-            SELECT 
-                m.nombre AS materia,
-                n.periodo,
-                n.nota1 AS "Evaluación 1",
-                n.nota2 AS "Evaluación 2",
-                n.nota3 AS "Evaluación 3",
-                n.nota4 AS "Evaluación 4",
-                n.nota5 AS "Evaluación 5",
-                n.porcentaje_nota1 AS "P1",
-                n.porcentaje_nota2 AS "P2",
-                n.porcentaje_nota3 AS "P3",
-                n.porcentaje_nota4 AS "P4",
-                n.porcentaje_nota5 AS "P5",
-                ROUND((
-                    (n.nota1 * n.porcentaje_nota1 / 100) + 
-                    (n.nota2 * n.porcentaje_nota2 / 100) + 
-                    (n.nota3 * n.porcentaje_nota3 / 100) + 
-                    (n.nota4 * n.porcentaje_nota4 / 100) + 
-                    (n.nota5 * n.porcentaje_nota5 / 100)
-                ), 2) AS "Nota Final",
-                a.asistencia_porcentaje AS "Asistencia (%)"
-            FROM 
-                notas n
-            INNER JOIN 
-                matriculas ma ON n.matricula_id = ma.id
-            INNER JOIN 
-                materias m ON ma.materia_id = m.id
-            INNER JOIN 
-                usuarios u ON ma.alumno_id = u.id
-            LEFT JOIN 
-                asistencias a ON a.matricula_id = ma.id
-            WHERE 
-                u.id = ? AND m.id = ?', [$alumno_id, $materia_id]);
+    $notas = DB::select('
+        SELECT 
+            m.nombre AS materia,
+            n.periodo,
+            n.nota1 AS "Evaluación 1",
+            n.nota2 AS "Evaluación 2",
+            n.nota3 AS "Evaluación 3",
+            n.nota4 AS "Evaluación 4",
+            n.nota5 AS "Evaluación 5",
+            n.porcentaje_nota1 AS "P1",
+            n.porcentaje_nota2 AS "P2",
+            n.porcentaje_nota3 AS "P3",
+            n.porcentaje_nota4 AS "P4",
+            n.porcentaje_nota5 AS "P5",
+            ROUND((
+                (n.nota1 * n.porcentaje_nota1 / 100) + 
+                (n.nota2 * n.porcentaje_nota2 / 100) + 
+                (n.nota3 * n.porcentaje_nota3 / 100) + 
+                (n.nota4 * n.porcentaje_nota4 / 100) + 
+                (n.nota5 * n.porcentaje_nota5 / 100)
+            ), 2) AS "Nota Final",
+            a.asistencia_porcentaje AS "Asistencia (%)"
+        FROM 
+            notas n
+        INNER JOIN 
+            matriculas ma ON n.matricula_id = ma.id
+        INNER JOIN 
+            materias m ON ma.materia_id = m.id
+        INNER JOIN 
+            usuarios u ON ma.alumno_id = u.id
+        LEFT JOIN 
+            asistencias a ON a.matricula_id = ma.id
+        WHERE 
+            u.id = ? AND m.id = ?', [$alumno_id, $materia_id]);
 
-        if (empty($notas)) {
-    return view('profesor.no_notas', ['id' => $materia_id]); // o ['id' => $id] según tu variable
-}
-
-        
-    
-        return view('profesor.vernotas', compact('notas'));
+    if (empty($notas)) {
+        return view('profesor.no_notas', ['id' => $materia_id]); // o ['id' => $id] según tu variable
     }
 
-    public function guardarNotas(Request $request, $id) {
-        $data = $request->only(['matricula_id', 'nota1', 'nota2', 'nota3', 'nota4', 'nota5', 'porcentaje_nota1', 'porcentaje_nota2', 'porcentaje_nota3', 'porcentaje_nota4', 'porcentaje_nota5']);
-        
-        DB::update('UPDATE notas SET nota1 = ?, nota2 = ?, nota3 = ?, nota4 = ?, nota5 = ?, porcentaje_nota1 = ?, porcentaje_nota2 = ?, porcentaje_nota3 = ?, porcentaje_nota4 = ?, porcentaje_nota5 = ? WHERE matricula_id = ? AND periodo = ?', [
-            $data['nota1'], $data['nota2'], $data['nota3'], $data['nota4'], $data['nota5'], $data['porcentaje_nota1'], $data['porcentaje_nota2'], $data['porcentaje_nota3'], $data['porcentaje_nota4'], $data['porcentaje_nota5'], $data['matricula_id'], $request->input('periodo')
+    return view('profesor.vernotas', compact('notas', 'materia_id', 'alumno_id'));
+    }
+
+
+    public function guardarNotas(Request $request, $materia_id, $alumno_id) {
+    $periodos = array_keys($request->input('nota1'));
+
+    foreach ($periodos as $periodo) {
+        // Si un porcentaje está vacío, puedes establecer un valor predeterminado o manejarlo según lo necesites
+        $porcentaje_nota1 = $request->input('porcentaje_nota1', 0);
+        $porcentaje_nota2 = $request->input('porcentaje_nota2', 0);
+        $porcentaje_nota3 = $request->input('porcentaje_nota3', 0);
+        $porcentaje_nota4 = $request->input('porcentaje_nota4', 0);
+        $porcentaje_nota5 = $request->input('porcentaje_nota5', 0);
+
+        DB::update('UPDATE notas SET 
+            nota1 = ?, 
+            nota2 = ?, 
+            nota3 = ?, 
+            nota4 = ?, 
+            nota5 = ?, 
+            porcentaje_nota1 = ?, 
+            porcentaje_nota2 = ?, 
+            porcentaje_nota3 = ?, 
+            porcentaje_nota4 = ?, 
+            porcentaje_nota5 = ? 
+            WHERE matricula_id = (SELECT id FROM matriculas WHERE alumno_id = ? AND materia_id = ?) AND periodo = ?', [
+            $request->input("nota1.$periodo"), 
+            $request->input("nota2.$periodo"), 
+            $request->input("nota3.$periodo"), 
+            $request->input("nota4.$periodo"), 
+            $request->input("nota5.$periodo"),
+            $porcentaje_nota1, 
+            $porcentaje_nota2, 
+            $porcentaje_nota3, 
+            $porcentaje_nota4, 
+            $porcentaje_nota5,
+            $alumno_id, $materia_id, $periodo
         ]);
 
-        return redirect()->route('docente.alumnos', ['id' => $id]);
+        // Actualizar asistencia si está presente
+        if ($request->has("asistencia.$periodo")) {
+            DB::update('UPDATE asistencias SET asistencia_porcentaje = ? WHERE matricula_id = (SELECT id FROM matriculas WHERE alumno_id = ? AND materia_id = ?)', [
+                $request->input("asistencia.$periodo"), $alumno_id, $materia_id
+            ]);
+        }
     }
 
+    return redirect()->route('docente.alumnos', ['id' => $materia_id])->with('success', 'Notas y porcentajes actualizados correctamente');
+}
 
 
 }
--- resources/views/profesor/vernotas.blade.php
+++ resources/views/profesor/vernotas.blade.php
@@ -4,33 +4,60 @@
     <div class="container">
         <h1 class="text-center">Notas del Alumno</h1>
         @foreach ($notas as $nota)
-            <table class="table">
-                <thead>
-                    <tr>
-                        <th>Periodo</th>
-                        <th>Evaluación 1 <br>{{ $nota->{'P1'} }}(%)</th>
-                        <th>Evaluación 2 <br>{{ $nota->{'P1'} }}(%)</th>
-                        <th>Evaluación 3 <br>{{ $nota->{'P1'} }}(%)</th>
-                        <th>Evaluación 4 <br>{{ $nota->{'P1'} }}(%)</th>
-                        <th>Evaluación 5 <br>{{ $nota->{'P1'} }}(%)</th>
-                        <th>Nota Final <br>100(%)</th>
-                        <th>Asistencia (%)</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>{{ $nota->periodo }}</td>
-                        <td>{{ $nota->{"Evaluación 1"} }}</td>
-                        <td>{{ $nota->{"Evaluación 2"} }}</td>
-                        <td>{{ $nota->{"Evaluación 3"} }}</td>
-                        <td>{{ $nota->{"Evaluación 4"} }}</td>
-                        <td>{{ $nota->{"Evaluación 5"} }}</td>
-                        <td>{{ $nota->{"Nota Final"} }}</td>
-                        <td>{{ $nota->{"Asistencia (%)"} }}</td>
-                    </tr>
-                </tbody>
-            </table>
+            <form action="{{ route('docente.notas.guardar', ['id' => $materia_id, 'alumno_id' => $alumno_id]) }}"
+                method="POST">
+                @csrf
+                <table class="table table-sm">
+                    <thead>
+                        <tr>
+                            <th>Periodo</th>
+                            <th>Evaluación 1 <br>{{ $nota->{'P1'} }}(%)</th>
+                            <th>Evaluación 2 <br>{{ $nota->{'P2'} }}(%)</th>
+                            <th>Evaluación 3 <br>{{ $nota->{'P3'} }}(%)</th>
+                            <th>Evaluación 4 <br>{{ $nota->{'P4'} }}(%)</th>
+                            <th>Evaluación 5 <br>{{ $nota->{'P5'} }}(%)</th>
+                            <th>Nota Final <br>100(%)</th>
+                            <th>Asistencia (%)</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>{{ $nota->periodo }}</td>
+                            <td><input type="text" name="nota1[{{ $nota->periodo }}]" value="{{ $nota->{"Evaluación 1"} }}"
+                                    class="form-control form-control-sm"></td>
+                            <td><input type="text" name="nota2[{{ $nota->periodo }}]"
+                                    value="{{ $nota->{"Evaluación 2"} }}" class="form-control form-control-sm"></td>
+                            <td><input type="text" name="nota3[{{ $nota->periodo }}]"
+                                    value="{{ $nota->{"Evaluación 3"} }}" class="form-control form-control-sm"></td>
+                            <td><input type="text" name="nota4[{{ $nota->periodo }}]"
+                                    value="{{ $nota->{"Evaluación 4"} }}" class="form-control form-control-sm"></td>
+                            <td><input type="text" name="nota5[{{ $nota->periodo }}]"
+                                    value="{{ $nota->{"Evaluación 5"} }}" class="form-control form-control-sm"></td>
+                            <td>{{ $nota->{"Nota Final"} }}</td>
+                            <td><input type="text" name="asistencia[{{ $nota->periodo }}]"
+                                    value="{{ $nota->{"Asistencia (%)"} }}" class="form-control form-control-sm"></td>
+                        </tr>
+                        <tr>
+                            <td>Porcentaje</td>
+                            <td><input type="text" name="porcentaje_nota1" value="{{ $nota->{'P1'} }}"
+                                    class="form-control form-control-sm"></td>
+                            <td><input type="text" name="porcentaje_nota2" value="{{ $nota->{'P2'} }}"
+                                    class="form-control form-control-sm"></td>
+                            <td><input type="text" name="porcentaje_nota3" value="{{ $nota->{'P3'} }}"
+                                    class="form-control form-control-sm"></td>
+                            <td><input type="text" name="porcentaje_nota4" value="{{ $nota->{'P4'} }}"
+                                    class="form-control form-control-sm"></td>
+                            <td><input type="text" name="porcentaje_nota5" value="{{ $nota->{'P5'} }}"
+                                    class="form-control form-control-sm"></td>
+                            <td></td>
+                            <td></td>
+                        </tr>
+                    </tbody>
+                </table>
+                <div class="volver-btn-container">
+                    <button type="submit" class="imprimir-btn">Guardar Cambios de Periodo {{ $nota->periodo }}</button>
+                </div>
+            </form>
         @endforeach
-
     </div>
 @endsection
